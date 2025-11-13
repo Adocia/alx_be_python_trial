bd5a8e73826298091eb24175e758d762e1f8dd6b
Docia Asafo-Adjei <adocia30@gmail.com>
2025-11-13 12:57:56 -0800

    git commit -m "basic_operation.py"

--- python_introduction/basic_operation.py
+++ python_introduction/basic_operation.py
@@ -1,7 +1,7 @@
-number1 = 10
-number2 = 5
-sum_results = number1 + number2
-print("sum_results")
-sum_results
-print(sum_results)
-15
+
+difference_results = number1 - number2
+print(difference_results)
+5
+product_results = number1 * number2
+print(product_results)
+50
